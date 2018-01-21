be93d4ab2b87ab38a797e91e73205c6eaf8b02e2
Alex M <merisanu.alexandru@outlook.com>
2018-01-21 19:17:52 +0200

    Added install_deps script

---
+++ scripts/linux/install_deps.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo dnf install nasm libbluray-devel curl-devel pulseaudio-libs-devel mpg123-devel libsndfile libsndfile-devel gstreamer1 gstreamer1-devel gstreamer1-plugins-base-devel gstreamer1-plugins-bad-free-devel libXrandr-devel libXi-devel libXcursor-devel
